65826b0e50f448689db0a1d0fc55a8d96b22f400
Rafael Pereira de Oliveira <rafael.oliveira4@alunos.unis.edu.br>
2022-09-20 21:58:53 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -13,13 +13,20 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 35);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+try
+{
+    Reserva reserva = new Reserva(diasReservados: 9);
+    reserva.CadastrarSuite(suite);
+    reserva.CadastrarHospedes(hospedes);
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao cadastrar reserva: {ex.Message}");
+}
 
 // Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
